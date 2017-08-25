bc160f168ddbc5d0920b2ebaad4cedbf1207677f
githubrandho <randhirkumar.093001@gmail.com>
2017-08-25 20:44:37 +0530

    Add project files.

---
+++ Angular2MVCDemo/Controllers/BaseAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Angular2MVCDemo.DBContext;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+
+namespace Angular2MVCDemo.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ Angular2MVCDemo/app/Components/home.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    template: `<img src="../../images/User.png" style="text-align:center"/>`
+})
+
+export class HomeComponent {
+}
